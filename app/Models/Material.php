9fda35f1b979f4311a88a7c9d276b33f20364824
adeirfanimanda <adeheaven09@gmail.com>
2024-05-21 17:34:06 +0700

    menambahkan model tabel material

---
+++ app/Models/Material.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Material extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function scopeSearching($query, $keyword)
+    {
+        $query->when($keyword, function ($query, $keyword) {
+            return $query->where('title', 'like', '%' . $keyword . '%');
+        });
+    }
+
+    // users
+    public function scopeSearching2($query, $keyword)
+    {
+        $query->when($keyword, function ($query, $keyword) {
+            return $query->where('title', 'like', '%' . $keyword . '%');
+        });
+    }
+}
