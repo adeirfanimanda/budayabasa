8d3fc7a450a720b0e15aba6dc80e92359f5678fc
adeirfanimanda <adeheaven09@gmail.com>
2024-05-22 06:47:04 +0700

    menambahkan script js untuk data materi - admin

---
+++ public/assets/js/datamateri/index.js
@@ -0,0 +1,33 @@
+// Membersihkan form saat tombol cancel diklik
+$(".cancelModalAddMateri, .cancelModalEditMateri").on("click", function () {
+    // Reset form modal
+    $(".modalAdminMateri")[0].reset();
+
+    // Remove invalid feedback and reset input values
+    var inputs = $("#formModalAdminMateri #title, #formModalAdminMateri #deskripsi, #formModalAdminMateri #document, #formEditModalAdminMateri #titleEdit, #formEditModalAdminMateri #deskripsiEdit, #formEditModalAdminMateri #documentEdit");
+    inputs.removeClass("is-invalid invalid-feedback").val("");
+});
+
+// Memuat data ke dalam form edit saat tombol edit diklik
+$(".buttonEditMateri").on("click", function () {
+    const code = $(this).data("code-materi");
+    const title = $(this).data("title-materi");
+    const desc = $(this).data("desc-materi");
+    const statusMateri = $(this).data("status-materi");
+    statusMateri == "Aktif"
+        ? $("#aktif").attr("selected", true)
+        : $("#nonaktif").attr("selected", true);
+    $(".codeMateri").val(code);
+    $("#titleEdit").val(title);
+    $("#deskripsiEdit").val(desc);
+    $("#formEditModalAdminMateri").modal("show");
+});
+
+// Menampilkan konfirmasi penghapusan saat tombol delete diklik
+$(".buttonDeleteMateri").on("click", function () {
+    const data = $(this).data("title-materi");
+    const code = $(this).data("code-materi");
+    $(".materiMessagesDelete").html(`Anda yakin ingin menghapus data materi dengan nama <strong>'${data}'</strong> ?`);
+    $(".codeMateri").val(code);
+    $("#deleteMateriConfirm").modal("show");
+});
