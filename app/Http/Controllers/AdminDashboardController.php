70077e568592ed9fd2e29ef25acc4cf024e0a982
adeirfanimanda <adeheaven09@gmail.com>
2024-06-17 16:20:01 +0700

    fix AdminDashboardController

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Charts\AnswerQuizChart;
-use App\Models\Quiz;
 use App\Models\User;
-use Illuminate\Http\Request;
-use App\Models\Result;
 use App\Models\Thread;
 use App\Models\Application;
+use App\Models\Dictionary;
+use App\Models\Material;
 
 class AdminDashboardController extends Controller
 {
@@ -17,12 +16,12 @@ class AdminDashboardController extends Controller
         return view('admin.dashboard.index', [
             'app' => Application::all(),
             'title' => 'Dashboard',
-            'totalQuiz' => Quiz::count(),
+            'totalDictionary' => Dictionary::count(),
+            'totalMaterial' => Material::count(),
             'totalLakiLaki' => User::where('gender', 'Laki-Laki')->where('is_admin', '!=', 1)->count(),
             'totalPerempuan' => User::where('gender', 'Perempuan')->where('is_admin', '!=', 1)->count(),
             'members' => User::where('is_admin', 0)->latest()->take(4)->select('name', 'image', 'created_at')->get(),
             'totalMember' => User::where('is_admin', false)->count(),
-            'answersQuiz' => Result::all()->count(),
             'threads' => Thread::all()->count(),
             'chart' => $chart->build()
         ]);
